f3dc526d9aaf26ec1a8503d12711a39520cd54e5
magikitty <elina.satola@gmail.com>
2020-07-14 14:56:33 +0300

    Modify getQuitInput, callMenuFunctions and main to implement quitting program

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"meal-planner/utils"
+	"strings"
 
 	"github.com/magikitty/menu"
 )
 
 func main() {
 	fmt.Println(utils.WelcomeMessage)
-	mainMenu()
+
+	for utils.QuitProgram == false {
+		mainMenu()
+	}
 }
 
 func mainMenu() {
@@ -26,7 +30,12 @@ func callMenuFunctions(menuSelection string) {
 	case utils.MenuMainOptions["2"]:
 		addNewMeal()
 	case utils.MenuMainOptions["3"]:
-		getQuitInput()
+		if getQuitInput() == utils.QuitYes {
+			utils.QuitProgram = true
+			fmt.Println("Quitting now. Bye bye!") // debugging
+		} else {
+			mainMenu()
+		}
 	default:
 		fmt.Println("Nothing to do") // debugging
 	}
@@ -40,6 +49,10 @@ func addNewMeal() {
 	fmt.Println("We are going to add a new meal! Yay!") // debugging
 }
 
-func getQuitInput() {
-	fmt.Println(utils.ConfirmQuit)
+func getQuitInput() (userInput string) {
+	for userInput != utils.QuitYes && userInput != utils.QuitNo {
+		fmt.Println(utils.ConfirmQuit)
+		userInput = utils.GetUserInput()
+	}
+	return strings.ToLower(userInput)
 }
