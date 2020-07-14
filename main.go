55d0bc3ed45268d69d4714bd78b7df8309517ef9
magikitty <elina.satola@gmail.com>
2020-07-14 13:25:10 +0300

    Add generateMealPlan, addNewMeal, getQuitInput and callMenuFunctions functions; modify mainMenu

--- main.go
+++ main.go
@@ -15,4 +15,31 @@ func main() {
 func mainMenu() {
 	fmt.Println(utils.MenuInstructions)
 	menu.PrintMenu(utils.MenuMainOptions, true)
+	menuSelection := menu.GetMenuSelection(utils.MenuMainOptions)
+	callMenuFunctions(menuSelection)
+}
+
+func callMenuFunctions(menuSelection string) {
+	switch menuSelection {
+	case utils.MenuMainOptions["1"]:
+		generateMealPlan()
+	case utils.MenuMainOptions["2"]:
+		addNewMeal()
+	case utils.MenuMainOptions["3"]:
+		getQuitInput()
+	default:
+		fmt.Println("Nothing to do") // debugging
+	}
+}
+
+func generateMealPlan() {
+	fmt.Println("We are going to generate a meal plan! Yay!") // debugging
+}
+
+func addNewMeal() {
+	fmt.Println("We are going to add a new meal! Yay!") // debugging
+}
+
+func getQuitInput() {
+	fmt.Println(utils.ConfirmQuit)
 }
