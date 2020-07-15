9a30bcf099d2d60bba12c9fb3a0f931164c42283
magikitty <elina.satola@gmail.com>
2020-07-15 10:17:51 +0300

    Review changes task001-001: Remove old publicly exposed vars and comments, modify MenuMessages

--- utils/constants.go
+++ utils/constants.go
@@ -1,21 +1,12 @@
 package utils
 
-/* decide if you prefer this pattern for storing messages in a struct
-benefits:
-1. messages are not publically exposed variables which can be accidentally changed in code
-2. accessing is easy; simply call utils.MenuMessages(). the desired message, example:
-	utils.MenuMessages().MenuInstructions for menu instructions
-if you adopt this, be sure to:
-	a. replace all calls to publically exposed vars with calls to MenuMessages()
-	b. remove old publically exposed vars
-*/
-
 // MenuMessages function returning struct object with menu messages
 func MenuMessages() menuMessages {
 	menuMessages := menuMessages{}
 	menuMessages.WelcomeMessage = "Welcome to the Meal Planner!"
 	menuMessages.MenuInstructions = "\nWhat do you want to do? Press the number of your choice."
 	menuMessages.MenuMainOptions = MenuMainOptions
+	menuMessages.MealPlanDuration = "How many days do you want to create a meal plan for?"
 	menuMessages.ConfirmQuit = "Are you sure you want to quit? y/n"
 	menuMessages.QuitYes = "y"
 	menuMessages.QuitNo = "n"
@@ -26,16 +17,11 @@ type menuMessages struct {
 	WelcomeMessage   string
 	MenuInstructions string
 	MenuMainOptions  map[string]string
+	MealPlanDuration string
 	ConfirmQuit      string
 	QuitYes          string
 	QuitNo           string
 }
-
-// WelcomeMessage displays when user starts program
-var WelcomeMessage = "Welcome to the Meal Planner!"
-
-// MenuInstructions explains how to make a choice in the menu
-var MenuInstructions = "\nWhat do you want to do? Press the number of your choice."
 
 // MenuMainOptions contains options for the main menu
 var MenuMainOptions = map[string]string{
@@ -44,17 +30,5 @@ var MenuMainOptions = map[string]string{
 	"3": "Quit",
 }
 
-// ConfirmQuit is a message for confirmation when quitting
-var ConfirmQuit = "Are you sure you want to quit? y/n"
-
-// QuitYes is user's confirmation to quit program
-var QuitYes = "y"
-
-// QuitNo is user's confirmation not to quit program
-var QuitNo = "n"
-
 // QuitProgram is a bool that is set to true when user wants to quit program
 var QuitProgram = false
-
-// MealPlanDuration for length of meal plan
-var MealPlanDuration = "How many days do you want to create a meal plan for?"
