9c5ca9d79aad972367db310d85165d9c4e1c6b85
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2020-10-10 01:30:38 +0300

    move meal plan generation functionality to own file

--- src/utils/meal-plan.go
+++ src/utils/meal-plan.go
@@ -1,61 +1,9 @@
 package utils
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
-
-// getMealPlanDuration returns number of days meal plan must last
-func getMealPlanDuration() (durationInputInt int) {
-	fmt.Println(MenuMessages().MealPlanDuration)
-	durationInput, err := GetInputAsInt()
-	if err != nil {
-		return getMealPlanDuration()
-	}
-	return durationInput
-}
-
-func checkDurationValid(mealPlanDuration int, totalPortions int) (durationValid bool) {
-	durationValid = false
-	if mealPlanDuration > totalPortions {
-		fmt.Println(MenuMessages().DurationNotValid)
-	} else {
-		durationValid = true
-	}
-	return durationValid
-}
-
-// return sum of portions of all meals
-func getSumAllMealsPortions(meals AllMeals) (sumAllPortions int) {
-	sumAllPortions = 0
-	for _, meal := range meals.Meals {
-		sumAllPortions += meal.Portions
-	}
-	return sumAllPortions
-}
-
-// makeMealPlan returns collection of random meals of duration length
-func makeMealPlan(duration int, allMealsSlice []Meal) []Meal {
-	var mealPlan []Meal
-	for i := 0; i < duration; {
-		randomNum := getRandomMealIndex(allMealsSlice)
-		randomMeal := allMealsSlice[randomNum]
-
-		if randomMeal.Portions > 1 && randomMeal.Portions <= (duration-i) {
-			mealPlan = append(mealPlan, addAllPortionsOfMeal(randomMeal)...)
-			i = i + randomMeal.Portions
-		} else if randomMeal.Portions == 1 {
-			mealPlan = append(mealPlan, randomMeal)
-			i++
-		} else {
-			fmt.Println(MenuMessages().MealPlanCreationFailed)
-			MenuMain()
-		}
-		allMealsSlice = removeMeal(allMealsSlice, randomNum)
-	}
-	return mealPlan
-}
 
 // getRandomMealIndex returns random valid index of slice
 func getRandomMealIndex(allMealsSlice []Meal) int {
