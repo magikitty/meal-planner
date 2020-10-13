2e530badf8d0e6374e561c3367a3759f66020158
magikitty <elina.satola@gmail.com>
2020-10-13 14:21:08 +0300

    Add TestGenerateMeals_durationValid

--- src/generatemeals/generatemeals_test.go
+++ src/generatemeals/generatemeals_test.go
@@ -17,3 +17,10 @@ func TestGenerateMeals_mealFitsPlan(t *testing.T) {
 	assert.Equal(t, expectedBoolFits, gotBoolFits, "Meal portions should be less than or equal to duration")
 	assert.Equal(t, expectedBoolDoesNotFit, gotBoolDoesNotFit, "Meal portions should be less than or equal to duration")
 }
+
+func TestGenerateMeals_durationValid(t *testing.T) {
+	expectedDurationValidBool := true
+	gotDurationValidBool := durationValid(8)
+
+	assert.Equal(t, expectedDurationValidBool, gotDurationValidBool, "Duration needs to be more than 0")
+}
