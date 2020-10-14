a6d831e4e8ddc21ea7fbb9bb23cac653b82d57fd
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2020-10-14 17:20:15 +0300

    Add unit test for get file data

--- src/utils/helpers_test.go
+++ src/utils/helpers_test.go
@@ -12,3 +12,10 @@ func TestHelpers_ConvertStringToInt(t *testing.T) {
 
 	assert.Equal(t, intExpected, intFromStringSucceess, "Failed to convert number string to int")
 }
+
+func TestHelpers_GetFileData(t *testing.T) {
+	byteArrayExpected := []byte{0x7b, 0xa, 0x20, 0x20, 0x20, 0x20, 0x22, 0x6b, 0x65, 0x79, 0x22, 0x3a, 0x22, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0xa, 0x7d}
+	byteArrayActual := GetFileData("../../data/tests/test-getfiledata.json")
+
+	assert.Equal(t, byteArrayExpected, byteArrayActual, "Failed to get expected byte array from file")
+}
