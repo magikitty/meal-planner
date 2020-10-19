abe462f654bc23d41c1f9b96bc5bfcaaaf2afd92
magikitty <elina.satola@gmail.com>
2020-10-19 14:40:15 +0300

    Add getIngredientUnitsAsString, refactor getIngredientsAsString

--- src/generatemeals/parse-meals.go
+++ src/generatemeals/parse-meals.go
@@ -17,22 +17,29 @@ func getMealParametersAsString(meal Meal) (string, string, string) {
 func getIngredientsAsString(meal Meal) string {
 	var ingredients []string
 	var ingredientString string
+
 	for _, ingredient := range meal.Ingredients {
 		 if ingredient.Unit != nil {
-			if ingredient.Unit.Unit != "" {
-				ingredientString = utils.Tab + utils.Tab + strconv.Itoa(ingredient.Quantity) + " " +
-					ingredient.Unit.Name + " (" + strconv.Itoa(ingredient.Unit.Quantity) + " " +
-					ingredient.Unit.Unit + ") " + ingredient.Name
-				ingredients = append(ingredients, ingredientString)
+			 ingredientString = getIngredientUnitsAsString(ingredient)
+			 ingredients = append(ingredients, ingredientString)
 			} else {
-				ingredientString = utils.Tab + utils.Tab + strconv.Itoa(ingredient.Quantity) + " " +
-					ingredient.Unit.Name + " " + ingredient.Name
-				ingredients = append(ingredients, ingredientString)
-			}
-		} else {
 			ingredientString = utils.Tab + utils.Tab + strconv.Itoa(ingredient.Quantity) + " " + ingredient.Name
 			ingredients = append(ingredients, ingredientString)
 		}
 	}
 	return strings.Join(ingredients, "\n")
+}
+
+func getIngredientUnitsAsString(ingredient Ingredient) string {
+	var ingredientString string
+
+	if ingredient.Unit.Unit != "" && ingredient.Unit.Quantity != 0 {
+		ingredientString = utils.Tab + utils.Tab + strconv.Itoa(ingredient.Quantity) + " " +
+			ingredient.Unit.Name + " (" + strconv.Itoa(ingredient.Unit.Quantity) + " " +
+			ingredient.Unit.Unit + ") " + ingredient.Name
+	} else {
+		ingredientString = utils.Tab + utils.Tab + strconv.Itoa(ingredient.Quantity) + " " +
+			ingredient.Unit.Name + " " + ingredient.Name
+	}
+	return ingredientString
 }
