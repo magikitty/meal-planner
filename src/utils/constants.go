703acdd4dc59f4f142b655c9bdeb93993c795c7e
Jonathan Widdowson <jonathan.widdowson1@gmail.com>
2020-10-23 17:29:18 +0300

    Implement using constants for page files and text content displayed to user

--- src/utils/constants.go
+++ src/utils/constants.go
@@ -13,6 +13,13 @@ var MenuMainOptions = map[string]string{
 	"3": "Quit",
 }
 
+// FilePaths returns struct with file paths
+func FilePaths() filePaths {
+	filePaths := filePaths{}
+	filePaths.JSONMealsData = "./data/meals/meals.json"
+	return filePaths
+}
+
 // MenuMessages returns struct with menu messages
 func MenuMessages() menuMessages {
 	menuMessages := menuMessages{}
@@ -21,22 +28,51 @@ func MenuMessages() menuMessages {
 	menuMessages.DisplayPlanFormatting = "Day %v: %v\n%v%v\n"
 	menuMessages.InputNotValid = "Invalid input, please try again."
 	menuMessages.MealPlanDuration = "How many days do you want to create a meal plan for?"
-	menuMessages.MenuInstructions = "\nWhat do you want to do? Press the number of your choice.\n"
+	menuMessages.MenuInstructions = "\nWhat do you want to do?\n"
 	menuMessages.MenuMainOptions = MenuMainOptions
 	menuMessages.QuitYes = "y"
 	menuMessages.WelcomeMessage = "Welcome to the Meal Planner!"
 	return menuMessages
 }
 
-// FilePaths returns struct with file paths
-func FilePaths() filePaths {
-	filePaths := filePaths{}
-	filePaths.JSONMealsData = "./data/meals/meals.json"
-	return filePaths
+// MessagesHome returns strings for home page
+func MessagesHome() messagesHome {
+	Messages := messagesHome{}
+	Messages.WelcomeMessage = MenuMessages().WelcomeMessage
+	Messages.Instructions = MenuMessages().MenuInstructions
+	Messages.OptionNewPlan = MenuMainOptions["1"]
+	Messages.OptionNewRecipe = MenuMainOptions["2"]
+	return Messages
+}
+
+// PageAddresses contains all page addresses used in e.g. messages and links
+func PageAddresses() pageAddress {
+	Address := pageAddress{}
+	Address.Home = "/"
+	Address.NewPlan = "/new-plan"
+	Address.NewRecipe = "/new-recipe"
+	return Address
+}
+
+// UXConstants values used to display any content to user
+func UXConstants() uxConstants {
+	u := uxConstants{}
+	u.AddressHome = PageAddresses().Home
+	u.AddressNewPlan = PageAddresses().NewPlan
+	u.AddressNewRecipe = PageAddresses().NewRecipe
+	u.HomeMessageWelcome = MessagesHome().WelcomeMessage
+	u.HomeMessageInstructions = MessagesHome().Instructions
+	u.HomeButtonOptionsNewPlan = MessagesHome().OptionNewPlan
+	u.HomeButtonOptionsNewRecipe = MessagesHome().OptionNewRecipe
+	return u
 }
 
 // ErrInvalidIndexToRemove returns error
 var ErrInvalidIndexToRemove = errors.New("cannot remove item as index invalid")
+
+type filePaths struct {
+	JSONMealsData string
+}
 
 type menuMessages struct {
 	ConfirmQuit           string
@@ -48,10 +84,6 @@ type menuMessages struct {
 	MenuMainOptions       map[string]string
 	QuitYes               string
 	WelcomeMessage        string
-}
-
-type filePaths struct {
-	JSONMealsData string
 }
 
 // GetFormatStrings initialization function returns map of string formatting values
@@ -67,19 +99,25 @@ func GetFormatStrings() map[string]string {
 	return formatStrings
 }
 
-type pageMessagesHome struct {
+type messagesHome struct {
 	WelcomeMessage  string
 	Instructions    string
 	OptionNewPlan   string
 	OptionNewRecipe string
 }
 
-// MessagesHome returns strings for home page
-func MessagesHome() pageMessagesHome {
-	Messages := pageMessagesHome{}
-	Messages.WelcomeMessage = MenuMessages().WelcomeMessage
-	Messages.Instructions = MenuMessages().MenuInstructions
-	Messages.OptionNewPlan = MenuMainOptions["1"]
-	Messages.OptionNewRecipe = MenuMainOptions["2"]
-	return Messages
+type pageAddress struct {
+	Home      string
+	NewPlan   string
+	NewRecipe string
+}
+
+type uxConstants struct {
+	AddressHome                string
+	AddressNewPlan             string
+	AddressNewRecipe           string
+	HomeMessageWelcome         string
+	HomeMessageInstructions    string
+	HomeButtonOptionsNewPlan   string
+	HomeButtonOptionsNewRecipe string
 }
