d31544e10ea801404056284d28ddbcca889757f1
magikitty <elina.satola@gmail.com>
2020-10-24 12:30:02 +0300

    Move FormattedMealPlan and formattedMeal from menus.go to parse-meals.go; modify formatting for ingredient strings

--- src/generatemeals/menus.go
+++ src/generatemeals/menus.go
@@ -3,7 +3,6 @@ package generatemeals
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 
 	"github.com/magikitty/meal-planner/src/utils"
@@ -28,45 +27,6 @@ func menuMainSelection(menuSelection string) {
 	case utils.MenuMessages().MenuMainOptions["3"]:
 		quit()
 	}
-}
-
-func FormatMealPlan(mealPlan []Meal, err error) []MealStringified {
-	var formattedMealPlan []MealStringified
-
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		var name string
-		var ingredients []string
-		var portionSize, portionsLeft int
-
-		for i, meal := range mealPlan {
-
-			if portionsLeft == 0 {
-				name = meal.Name
-				ingredients = getIngredientsAsString(meal)
-				portionSize = meal.PortionSize
-				portionsLeft = portionSize
-			} else if portionsLeft != portionSize {
-				ingredients = []string{}
-			}
-
-			formattedMealPlan = append(formattedMealPlan, formattedMeal(i, name, ingredients, portionSize))
-			portionsLeft--
-		}
-	}
-
-	return formattedMealPlan
-}
-
-func formattedMeal(i int, name string, ingredients []string, portionSize int) MealStringified {
-	formattedMeal := MealStringified{
-		"Day " + strconv.Itoa(i+1) + ":",
-		name,
-		ingredients,
-		"Portion size: " + strconv.Itoa(portionSize)}
-
-	return formattedMeal
 }
 
 func quit() {
--- src/generatemeals/parse-meals.go
+++ src/generatemeals/parse-meals.go
@@ -1,46 +1,76 @@
 package generatemeals
 
 import (
+	"fmt"
 	"strconv"
-	"strings"
-
-	"github.com/magikitty/meal-planner/src/utils"
 )
 
-func prettifyMealProperties(meal Meal) (string, string, int) {
-	name := meal.Name
-	ingredients := getIngredientsAsString(meal)
-	portionSize := meal.PortionSize
+func FormatMealPlan(mealPlan []Meal, err error) []MealStringified {
+	var formattedMealPlan []MealStringified
 
-	return name, ingredients, portionSize
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		var name string
+		var ingredients []string
+		var portionSize, portionsLeft int
+
+		for i, meal := range mealPlan {
+
+			if portionsLeft == 0 {
+				name = meal.Name
+				ingredients = getIngredientsAsString(meal)
+				portionSize = meal.PortionSize
+				portionsLeft = portionSize
+			} else if portionsLeft != portionSize {
+				ingredients = []string{}
+			}
+
+			formattedMealPlan = append(formattedMealPlan, formattedMeal(i, name, ingredients, portionSize))
+			portionsLeft--
+		}
+	}
+
+	return formattedMealPlan
 }
 
-func getIngredientsAsString(meal Meal) string {
+func formattedMeal(i int, name string, ingredients []string, portionSize int) MealStringified {
+	formattedMeal := MealStringified{
+		"Day " + strconv.Itoa(i+1) + ":",
+		name,
+		ingredients,
+		"Portion size: " + strconv.Itoa(portionSize)}
+
+	return formattedMeal
+}
+
+func getIngredientsAsString(meal Meal) []string {
 	var ingredients []string
 	var ingredientString string
 
 	for _, ingredient := range meal.Ingredients {
-		if ingredient.Unit != nil {
+		if ingredient.Unit.Name != "" {
 			ingredientString = getIngredientUnitsAsString(ingredient)
 			ingredients = append(ingredients, ingredientString)
 		} else {
+			// Format ingredient for recipe string
 			ingredientString = strconv.Itoa(ingredient.Quantity) + " " + ingredient.Name
-			//ingredientString = utils.Tab + utils.Tab + strconv.Itoa(ingredient.Quantity) + " " + ingredient.Name
 			ingredients = append(ingredients, ingredientString)
 		}
 	}
-	return strings.Join(ingredients, "\n")
+	return ingredients
 }
 
 func getIngredientUnitsAsString(ingredient Ingredient) string {
 	var ingredientString string
 
+	// Format ingredient for recipe string
 	if ingredient.Unit.Unit != "" && ingredient.Unit.Quantity != 0 {
-		ingredientString = utils.Tab + utils.Tab + strconv.Itoa(ingredient.Quantity) + " " +
+		ingredientString = strconv.Itoa(ingredient.Quantity) + " " +
 			ingredient.Unit.Name + " (" + strconv.Itoa(ingredient.Unit.Quantity) + " " +
 			ingredient.Unit.Unit + ") " + ingredient.Name
 	} else {
-		ingredientString = utils.Tab + utils.Tab + strconv.Itoa(ingredient.Quantity) + " " +
+		ingredientString = strconv.Itoa(ingredient.Quantity) + " " +
 			ingredient.Unit.Name + " " + ingredient.Name
 	}
 	return ingredientString
